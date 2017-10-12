610035e8eb6e0f92ec3c95747496e8c9931c29ca
daswolke <wolke@weeb.sh>
2017-10-12 19:51:26 +0200

    start adding bare-bone event processing

--- src/EventProcessor.js
+++ src/EventProcessor.js
@@ -14,8 +14,68 @@ class EventProcessor {
 
     async process(event) {
         switch (event.t) {
+            case 'PRESENCE_UPDATE':
+                this.client.emit('presenceUpdate', event.d);
+                break;
             case 'MESSAGE_CREATE':
                 this.client.emit('messageCreate', event.d);
+                break;
+            case 'MESSAGE_UPDATE':
+                this.client.emit('messageUpdate', event.d);
+                break;
+            case 'MESSAGE_DELETE':
+                this.client.emit('messageDelete', event.d);
+                break;
+            case 'MESSAGE_DELETE_BULK':
+                this.client.emit('messageDeleteBulk', event.d);
+                break;
+            case 'MESSAGE_REACTION_ADD':
+                this.client.emit('messageReactionAdd', event.d);
+                break;
+            case 'MESSAGE_REACTION_REMOVE':
+                this.client.emit('messageReactionRemove', event.d);
+                break;
+            case 'GUILD_BAN_ADD':
+                this.client.emit('guildBanAdd', event.d);
+                break;
+            case 'GUILD_BAN_REMOVE':
+                this.client.emit('guildBanRemove', event.d);
+                break;
+            case 'GUILD_MEMBER_ADD':
+                this.client.emit('guildMemberAdd', event.d);
+                break;
+            case 'GUILD_MEMBER_REMOVE':
+                this.client.emit('guildMemberRemove', event.d);
+                break;
+            case 'CHANNEL_CREATE':
+                this.client.emit('channelCreate', event.d);
+                break;
+            case 'CHANNEL_UPDATE':
+                this.client.emit('channelUpdate', event.d);
+                break;
+            case 'CHANNEL_DELETE':
+                this.client.emit('channelDelete', event.d);
+                break;
+            case 'CHANNEL_PINS_UPDATE':
+                this.client.emit('channelPinUpdate', event.d);
+                break;
+            case 'GUILD_CREATE':
+                this.client.emit('guildCreate', event.d);
+                break;
+            case 'GUILD_UPDATE':
+                this.client.emit('guildUpdate', event.d);
+                break;
+            case 'GUILD_REMOVE':
+                this.client.emit('guildRemove', event.d);
+                break;
+            case 'GUILD_EMOJIS_UPDATE':
+                this.client.emit('guildEmojiUpdate', event.d);
+                break;
+            case 'USER_UPDATE':
+                this.client.emit('userUpdate', event.d);
+                break;
+            case 'VOICE_STATE_UPDATE':
+                this.client.emit('voiceStateUpdate', event.d);
                 break;
             default:
                 console.log(event);
